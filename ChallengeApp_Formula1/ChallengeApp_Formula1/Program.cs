f9984388fb1e73dd4444d9fb6a61777c8b9711b1
SebastianCiaglo <SebastianCiaglo@gmail.com>
2023-06-02 21:34:30 +0200

    Homework day 4

--- ChallengeApp_Formula1/ChallengeApp_Formula1/Program.cs
+++ ChallengeApp_Formula1/ChallengeApp_Formula1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+string name = "Ewa";
+bool isMale = true;
+int age = 33;
+string gender;
+
+if (isMale==true)
+{
+    gender="mężczyzna";
+}
+else
+{
+    gender = "kobieta";
+}
+
+if(age<30 && isMale==false)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name=="Ewa" && age==33)
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isMale == true && age <18)
+{
+    Console.WriteLine("Niepelnoletni "+gender);
+}
+else if (age < 30)
+{
+    Console.WriteLine(name + ", " + gender + " ponizej 30 lat");
+}
+else if (age > 30)
+{
+    Console.WriteLine(name + ", " + gender + " ponizej 30 lat");
+}
